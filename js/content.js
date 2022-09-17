e5d0822c2e66e297034d9de5701383321e0aecf6
Kartikey Tiwari <tiwari.kartik3002@gmail.com>
2022-09-17 23:24:46 +0530

    searchbar mobile

--- js/content.js
+++ js/content.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(function(){
         $("#bgcolor").load("https://getprojects.tech/components/bgcolor/index.html");
+        $("#searchMob").load("https://getprojects.tech/components/searchbar/index.html");
         $("#navbar").load("https://getprojects.tech/components/navbar/index.html");
         $("#footer").load("https://getprojects.tech/components/footer/index.html");
     });
